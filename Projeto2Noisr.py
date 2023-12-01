f004f3a52f5b857ed3ba019164a555a2a855dde3
Ricardo Ribeiro 109507 <ricardo.silva.ribeiro@tecnico.ulisboa.pt>
2023-12-01 22:06:52 +0000

    Código por preencher

--- Projeto2Noisr.py
+++ Projeto2Noisr.py
@@ -1,3 +1,25 @@
-#Projeto2Noisr 123
+#Projeto2Noisr
 
-print("Noisr")
+import random
+import matplotlib.pyplot as plt
+
+def random_walk(num_steps, prob_right, num_particles):
+    #escrever aqui
+    
+    create_plot(num_steps, particles_paths)
+    return particles_paths
+
+def create_plot(num_steps, particles_paths):
+    time = [x for x in range(len(particles_paths[0]))]
+    for particle_path in particles_paths:
+        plt.plot(particle_path, time)
+    plt.title("Random Walk - N particles")
+    plt.xlabel("Position")
+    plt.ylabel("Time")
+    plt.show()
+
+num_steps = 100
+prob_right = 0.5
+num_particles = 10
+
+random_walk(num_steps, prob_right, num_particles)
